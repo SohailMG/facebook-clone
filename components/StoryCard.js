d886f6869348c100552b5d8d173cd03bc3df0485
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-09-11 15:45:45 +0100

    added feed stories

---
+++ components/StoryCard.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+function StoryCard({ name, src, profile }) {
+  return (
+    <div className="relative h-14 w-14 md:h-20 md:w-20 lg:h-56 lg:w-32 cursor-pointer overflow-x p-3">
+      <Image
+        className="absolute opacity-0 lg:opacity-100 z-50 top-10 rounded-full"
+        src={profile}
+        alt={name}
+        width={40}
+        height={40}
+        objectFit="cover"
+      />
+      <Image
+        className="object-cover filter brightness-75 rounded-full lg:rounded-3xl"
+        src={src}
+        alt={name}
+        layout="fill"
+      />
+    </div>
+  );
+}
+
+export default StoryCard;
