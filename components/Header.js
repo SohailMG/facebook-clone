c468c1a4362eac841d20929a5b693e034cc2aff0
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-09-11 14:33:04 +0100

    added login with facebook

--- components/Header.js
+++ components/Header.js
@@ -14,10 +14,13 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import HeaderIcon from "./HeaderIcon";
+import { signout, signOut, useSession } from "next-auth/client";
+
 
 
 
 function Header() {
+    const [session] = useSession();
   return (
     <div className="flex sticky top-0 z-50 bg-white items-center p-2 lg:px-5 shadow-md">
       {/* Left Header */}
@@ -50,7 +53,16 @@ function Header() {
       {/* right Header */}
       <div className="flex items-center sm:space-x-2 justify-end">
           {/* Profile picture */}
-          <p className="whitespace-nowrap font-semibold pr-3">Sohail Gsais</p>
+          <Image
+          alt="Profile picture"
+          onClick={()=> signout()}
+          className="rounded-full cursor-pointer"
+          src={session.user.image}
+          width="40"
+          height="40"
+          layout="fixed"
+          />
+          <p className="whitespace-nowrap font-semibold pr-3">{session.user.name}</p>
           <ViewGridIcon className="icon"/>
           <ChatIcon className="icon"/>
           <BellIcon className="icon"/>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({session}) {
+  if(!session) return <Login/>
   return (
     <div>
       <Head>
@@ -10,12 +13,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
+      {/* Header*/}
       <Header/>
-
 
       <main>
         {/* Sidebar */}
+          
         {/* Feed */}
         {/* Widgets */}
       </main>
@@ -23,4 +26,15 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // GET USERS
+  const session = await getSession(context)
+  return {
+    props:{
+      session
+    }
+  }
+
 }
