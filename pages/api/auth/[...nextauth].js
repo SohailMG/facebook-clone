2b348968230797a7de3d638cd6a89b50be79d143
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-09-12 01:17:02 +0100

    fixed uri redirect

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,10 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Facebook({
-      clientId: "380619236931350",
-      clientSecret: "cf0ea126c62733ee19eff8dadf23038c",
-    }),
+   Providers.Facebook({
+       clientId: process.env.FACEBOOK_CLIENT_ID,
+       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+   })
+   
   ],
 });
